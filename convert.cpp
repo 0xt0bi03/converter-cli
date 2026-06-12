2213409485eadcf2a76efaf01c7a4f9b328cee63
Tobi <m4sk3d.t0bi@gmail.com>
2026-06-12 15:06:45 +0530

    first commit

---
+++ convert.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// clearing the screen.
+void clearScreen() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+unordered_map<string, double> length = {
+    {"mm", 0.001},
+    {"cm", 0.01},
+    {"dm", 0.1},
+    {"m", 1.0},
+    {"km", 1000.0},
+    {"in", 0.0254},
+    {"ft", 0.3048},
+    {"yd", 0.9144},
+    {"mi", 1609.344},
+    {"nmi", 1852.0},
+    {"um", 1e-6},
+    {"nm", 1e-9}
+};
+
+
+string conversion()
+{
+
+	return "";
+}
+
+
+int main()
+{
+	cout << "CONVERTER++" << endl;
+	cout << endl;
+
+	return 0;
+}
